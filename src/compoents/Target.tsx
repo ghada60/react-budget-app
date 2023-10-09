185fa820aaa3d51880ae5621e706fdbc6c4d8fc1
ghada <ghada.alnmla98@gmail.com>
2023-10-09 16:34:08 +0300

    react

---
+++ src/compoents/Target.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Target() {
+  return ( <div>
+        <form action="">
+            <label htmlFor="">set target</label>
+            <input type="number" />
+            <button>reset</button>
+        </form>
+
+    </div>
+  )
+}
